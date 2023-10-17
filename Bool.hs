8d4295802ed96b4df1ba557174fdc4beb6385d47
Raoni Silva <raoni.silvestre@hotmail.com>
2023-10-17 20:47:51 -0300

    if_then_else for α

--- Bool.hs
+++ Bool.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 module Bool where
 
 import Prelude hiding(True, False, Bool)
@@ -5,6 +6,6 @@ import Prelude hiding(True, False, Bool)
 data Bool = False | True
     deriving( Eq, Show)
 
-if_then_else :: Bool -> Bool -> Bool -> Bool
+if_then_else :: Bool -> a -> a -> a
 if_then_else True n _ = n
 if_then_else False _ m = m
