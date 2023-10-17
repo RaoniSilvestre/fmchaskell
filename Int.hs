96a755eccdf97593063ef8511bb16550d682bfbc
Raoni Silva <raoni.silvestre@hotmail.com>
2023-10-17 20:42:57 -0300

    update bad sum ints

--- Int.hs
+++ Int.hs
@@ -1,5 +1,6 @@
 module Int where
 
+
 import Prelude hiding(simp,soma,Int)
 
 data Int = O | S Int | P Int
@@ -14,5 +15,10 @@ simp x = x
 soma :: Int -> Int -> Int
 soma x O = x
 soma O x = x
+soma (P x) (S y) = soma x y
+soma (S x) (P y) = soma x y
 soma x (S y) = simp(S(soma x y))
 soma x (P y) = simp(P(soma x y))
+
+
+
