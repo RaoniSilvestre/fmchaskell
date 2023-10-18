9146cda38daa78d1d999d986b0cfc4980ba9f129
Raoni Silva <raoni.silvestre@hotmail.com>
2023-10-17 21:03:56 -0300

    List replicate

--- List.hs
+++ List.hs
@@ -1,9 +1,17 @@
 {-# LANGUAGE GADTs #-}
 
 module List where
-import Prelude hiding (Bool, True, False)
+import Prelude hiding (
+    replicate,
+    sum,mult, exp, min, max,
+    minimum, maximum,(<),(<=),
+    Bool, True, False)
 import Nat
 import Bool
 data List a = Empty | Cons a (List a)
     deriving( Eq, Show)
+
+replicate :: Nat -> a -> List a
+replicate O _ = Empty
+replicate (S n) a = Cons a (replicate n a)
 
